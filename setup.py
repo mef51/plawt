dd206e91a13d3a9b03277dfeb08424fefad0117c
mohammed chamma <hms.mohammed@gmail.com>
2017-01-20 22:02:39 -0500

    publish on pip holy cow https://pypi.python.org/pypi/plawt/
    ```
    python setup.py sdist
    twine upload dist/*
    ```
    
    when its the first time register with:
    `twine register dist/whaever.tar.gz`

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+	name = 'plawt',
+	version = '1.0.0',
+	description = 'JSON-like sugar for plotting with matplotlib',
+	long_description = 'JSON-like sugar for plotting with matplotlib',
+	author = 'Mohammed Chamma',
+	py_modules=["plawt"],
+	author_email = 'hms.mohammed@gmail.com',
+	license='MIT',
+	url = 'https://github.com/mef51/plawt',
+	keywords = 'plotting matplotlib science sugar wrapper',
+	classifiers = [],
+)
