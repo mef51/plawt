2f11d28a2bab2fa0a2f1adbb0ff6fa8d61f8b7f2
mohammed chamma <hms.mohammed@gmail.com>
2017-01-22 15:28:15 -0500

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name = 'plawt',
-	version = '1.0.1',
+	version = '1.0.2',
 	description = 'JSON-like sugar for plotting with matplotlib',
 	long_description = 'JSON-like sugar for plotting with matplotlib',
 	author = 'Mohammed Chamma',
